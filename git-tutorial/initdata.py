e1bb50dc1b3d7bd439a4adb7bf7ce80860a932b0
ValeevDmitriy <aldur@mail.ru>
2014-07-12 22:03:25 +1100

    added input/output db to files

--- git-tutorial/initdata.py
+++ git-tutorial/initdata.py
@@ -1,3 +1,5 @@
+#заполняем базу данных
+
 bob = {'name' : 'Bob Smith', 'age' : 42, 'pay' : 30000, 'job' : 'dev'}
 sue = {'name' : 'Sue Jones', 'age' : 50, 'pay' : 40000, 'job' : 'hdw'}
 tom = {'name' : 'Tom',       'age' : 50, 'pay' : 0,      'job' : None}
